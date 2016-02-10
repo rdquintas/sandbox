3f61d88262ba5610e622d1540933dd5ec471ae75
Ricardo Quintas <rqcenter-web@example.com>
2016-02-10 09:41:44 +0000

    trying thumbs

---
+++ thumb.js
@@ -0,0 +1,27 @@
+var thumbs = require('thumbs');
+
+var thumbnails = thumbs({
+    small: {
+        height: 256 * 2,
+        quality: 25
+    },
+    large: {
+        max: 1024 * 2,
+        quality: 50
+    },
+    huge: {
+        max: 1024 * 8,
+        quality: 50
+    }
+});
+
+console.log("start");
+
+thumbnails.write({
+    src: 'flower.jpg',
+    dest: 'thumb_flower.jpg'
+});
+
+thumbnails.on('data', function(obj) {
+    console.log('thumbnailed:', obj);
+});
---
+++ twitter.js
@@ -0,0 +1,29 @@
+var Twitter = require('twitter');
+
+var client = new Twitter({
+    consumer_key: process.env.TWITTER_CONSUMER_KEY,
+    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+    access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
+    request_options: {
+        proxy: process.env.ZRQ_PROXY
+    }
+});
+
+
+var params = {
+    screen_name: 'ricquintas'
+};
+
+client.get('statuses/user_timeline', params, function(error, tweets, response) {
+    if (!error) {
+        for (var i = tweets.length - 1; i >= 0; i--) {
+            var obj = tweets[i].entities.media;
+            if (obj) {
+                console.log(obj[0].media_url);
+            }
+        }
+    } else {
+        console.log(error);
+    }
+});
